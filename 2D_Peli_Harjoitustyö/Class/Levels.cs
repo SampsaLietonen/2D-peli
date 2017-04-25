082081af8d2fb1ab210650902ee3e013ffa8b6cf
fnuzon <nikotamminen2@hotmail.com>
2017-04-25 15:53:08 +0300

    testi

---
+++ 2D_Peli_Harjoitustyö/Class/Levels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Peli_Harjoitustyö.Class
+{
+    class Levels
+    {       
+
+        public static void LevelManager(int Level)
+        {
+            if (Level == 1)
+            {
+                if (MainPage.enemyAdded < 5)
+                {
+                    return;
+                }
+                else
+                {
+                    MainPage.EnemyTimer.Stop();
+                    if (MainPage.enemyXPOS.Count == 0)
+                    {
+                        MainPage.countdown = 0;
+                    }                   
+                }          
+            }
+        }
+    }
+}
--- 2D_Peli_Harjoitustyö/MainPage.xaml.cs
+++ 2D_Peli_Harjoitustyö/MainPage.xaml.cs
@@ -2,39 +2,35 @@
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.System;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.UI.Core;
 using _2D_Peli_Harjoitustyö.Class;
 using Windows.UI;
+using Windows.UI.Xaml.Media;
 using System.Numerics;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace _2D_Peli_Harjoitustyö
 {
-   
+
     public sealed partial class MainPage : Page
     {
-        public static CanvasBitmap BG, StartScreen, Level1, ScoreScreen, Bullet, Enemy1, Enemy2, ENEMY_IMG, Player, Boom;
+        public static CanvasBitmap BG, StartScreen, Level1, ScoreScreen, Bullet, Enemy1, Enemy2, ENEMY_IMG, Player, Boom;        
         public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
         public static float DesignWidth = 1600;
         public static float DesignHeight = 1200;
         public static float scaleWidth, scaleHeight, pointX, pointY, bulletX, bulletY, MyScore, boomX, boomY;
         public static int boomCount = 60;   // frames
+        public static int enemyAdded = 0;
+        public static int Level = 0;
 
         public static int countdown = 60; // 60s roundtime
         public static bool RoundEnded = false;
@@ -57,87 +53,33 @@ namespace _2D_Peli_Harjoitustyö
 
         //Random Generators
         public Random EnemyTypeRand = new Random(); // enemy type 1 or 2
+       
         public Random EnemyGenRand = new Random(); // generation interval
 
         public Random EnemyXstart = new Random(); // random start position to all enemies
+       
 
-
-        //private Player player;
-
-               
-        // game loop timer
-        private DispatcherTimer timer;
-
-        // canvas width and height (used to randomize a new flower)
-        //private double CanvasWidth;
-        //private double CanvasHeight;
-
-        // which keys are pressed 
-        private bool UpPressed;
-        private bool LeftPressed;
-        private bool RightPressed;
-
-
-        /*// audio
-        private MediaElement mediaElement;*/
-
-        /// <summary>
-        /// Default constructor.
-        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
             Window.Current.SizeChanged += Current_SizeChanged;
             Scaling.SetScale();
-            bulletX = (float)bounds.Width / 2;
-            bulletY = (float)bounds.Height;
+            bulletX = (float)bounds.Width / 2 + -110;
+            bulletY = (float)bounds.Height + -230;
 
             RoundTimer.Tick += Roundtimer_Tick;
             RoundTimer.Interval = new TimeSpan(0, 0, 1);
 
             EnemyTimer.Tick += EnemyTimer_Tick;
             EnemyTimer.Interval = new TimeSpan(0, 0, 0, 0, EnemyGenRand.Next(300, 3000));
-        
-        
-
-            // change the default startup mode
-            /*ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-            
-            ApplicationView.PreferredLaunchViewSize = new Size(1600, 1200);*/
-
-            // key listeners
-            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
-            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
-
-            // get canvas width and height
-            /*CanvasWidth = MyCanvas.Width;
-            CanvasHeight = MyCanvas.Height;*/
-
-            
-            /*player = new Player
-            {
-                LocationX = CanvasWidth / 2,
-                LocationY = CanvasHeight / 2
-            };*/
-            
-
-            
-
-            // init audio
-           // InitAudio();
-
-            // game loop 
-            /*timer = new DispatcherTimer();
-            timer.Tick += Timer_Tick;
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
-            timer.Start();*/
-            
+                                                       
         }
+
 
         private void EnemyTimer_Tick(object sender, object e)
         {
             int ES = EnemyTypeRand.Next(1,3);
-            int SP = EnemyXstart.Next(0, (int)bounds.Width); // starting position X
+            int SP = EnemyXstart.Next(0, (int)bounds.Width); // starting position X            
             if (SP > bounds.Width / 2)
             {
                 enemyDIR.Add("left");
@@ -150,27 +92,16 @@ namespace _2D_Peli_Harjoitustyö
             enemyXPOS.Add(SP);
             enemyYPOS.Add(-50 * scaleHeight);
             enemyTYPE.Add(ES);
-
             EnemyTimer.Interval = new TimeSpan(0, 0, 0, 0, EnemyGenRand.Next(500, 2000));
+            enemyAdded++;
+
         }
 
         private void Roundtimer_Tick(object sender, object e)
         {
             countdown -= 1;
-            /*// move 
-            if (UpPressed) player.Move();
 
-            // rotate
-            if (LeftPressed) player.Rotate(-1);
-            if (RightPressed) player.Rotate(1);
-
-            // update
-            player.UpdateLocation();
-
-            // collision
-            //CheckCollision();*/
-
-            if (countdown < 1)
+            if(countdown < 1)
             {
                 RoundTimer.Stop();
                 RoundEnded = true;
@@ -179,69 +110,6 @@ namespace _2D_Peli_Harjoitustyö
 
 
 
-
-        /// <summary>
-        /// Game loop.
-        /// </summary>
-        /*private void Timer_Tick(object sender, object e)
-        {
-            // move 
-            if (UpPressed) player.Move();
-
-            // rotate
-            if (LeftPressed) player.Rotate(-1);
-            if (RightPressed) player.Rotate(1);
-
-            // update
-            player.UpdateLocation();
-
-            // collision
-            //CheckCollision();
-        }*/
-
-
-
-        /// <summary>
-        /// Check if some keys are pressed.
-        /// </summary>
-         private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
-         {
-             switch (args.VirtualKey)
-             {
-                 case VirtualKey.Up:
-                     UpPressed = true;                    
-                     break;
-                 case VirtualKey.Left:
-                     LeftPressed = true;
-                     break;
-                 case VirtualKey.Right:
-                     RightPressed = true;
-                     break;
-                 default:
-                     break;
-             }
-         }
-
-        /// <summary>
-        /// Check if some keys are released.
-        /// </summary>
-        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
-         {
-             switch (args.VirtualKey)
-             {
-                 case VirtualKey.Up:
-                     UpPressed = false;
-                     break;
-                 case VirtualKey.Left:
-                     LeftPressed = false;
-                     break;
-                 case VirtualKey.Right:
-                     RightPressed = false;
-                     break;
-                 default:
-                     break;
-             }
-         }
          
         private void Current_SizeChanged(object sender, WindowSizeChangedEventArgs e)
         {
@@ -263,7 +131,7 @@ namespace _2D_Peli_Harjoitustyö
             Level1 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/background.png"));
             ScoreScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/ScoreScreen.png"));
             Bullet = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/bullet.png"));
-            Player = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/hahmo.png"));
+            Player = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Player.png"));
             Enemy1 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Enemy1.png"));
             Enemy2 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Enemy2.png"));
             Boom = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Boom.png"));
@@ -273,6 +141,7 @@ namespace _2D_Peli_Harjoitustyö
         private void GameCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
             GameManager.GAMEMANAGER();
+            Levels.LevelManager(1);
             args.DrawingSession.DrawImage(Scaling.img(BG));
             args.DrawingSession.DrawText(countdown.ToString(), 100, 100, Colors.Yellow);
 
@@ -318,6 +187,7 @@ namespace _2D_Peli_Harjoitustyö
                 //Display projectiles
                 for (int i = 0; i < bulletXPOS.Count; i++) // every time new bullet is tapped to list -> shoot projectile
                 {
+                    // Percent increases the speed of the bullet from the canvas
                     pointX = (bulletX + (bulletXPOS[i] - bulletX) * percent[i]); //linear interpolation
                     pointY = (bulletY + (bulletYPOS[i] - bulletY) * percent[i]);
                     args.DrawingSession.DrawImage(Scaling.img(Bullet), pointX - (32 * scaleWidth), pointY -(32 * scaleHeight)); // delete half of the picture size to compensate the bullet and it will but the bullet right to the center of the tap
@@ -356,8 +226,8 @@ namespace _2D_Peli_Harjoitustyö
                     }
                 }
 
-                // Draw Player
-                args.DrawingSession.DrawImage(Scaling.img(Player), (float)bounds.Width / 2 - (139 * scaleWidth), (float)bounds.Height - (260 * scaleHeight));
+                // Draw Player                
+                args.DrawingSession.DrawImage(Scaling.img(Player), (float)bounds.Width / 2 - (258 * scaleWidth), (float)bounds.Height - (260 * scaleHeight));
                 
             }
 
